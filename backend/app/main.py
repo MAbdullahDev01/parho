75a5fe64663ed8bc8117671a749d13260a82a7f9
Muhammad Abdullah <abdullahiqbal2008@gmail.com>
2026-08-24 22:36:47 +0500

    feat: register admin verification API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.api.admin.route import router as admin_router
 from app.api.webhooks.router import router as webhooks_router
 from app.api.users.route import router as user_router
 from app.api.tutors.route import router as tutor_router
@@ -9,6 +10,8 @@ app = FastAPI(title="Parho API")
 app.include_router(webhooks_router)
 app.include_router(user_router)
 app.include_router(tutor_router)
+app.include_router(admin_router)
+
 
 @app.get("/health")
 def health_check():
