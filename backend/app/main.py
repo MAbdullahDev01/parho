127f5814a9cefc0391383ef7b36fe8067f8fcbb5
Muhammad Abdullah <abdullahiqbal2008@gmail.com>
2026-08-31 17:21:10 +0500

    feat: register wallet routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from app.api.admin.route import router as admin_router
 from app.api.bookings.route import router as bookings_router
 from app.api.messages.route import router as messages_router
+from app.api.wallet.route import router as wallet_router
 from app.api.webhooks.router import router as webhooks_router
 from app.api.users.route import router as user_router
 from app.api.tutors.route import router as tutor_router
@@ -24,6 +25,7 @@ app.include_router(tutor_router)
 app.include_router(tutor_discovery_router)
 app.include_router(bookings_router)
 app.include_router(messages_router)
+app.include_router(wallet_router)
 app.include_router(admin_router)
 
 
